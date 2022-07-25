eff3f2050615791d7d546b01dd6000ceec6e6db7
FN-TOKENS <84870847+dc199411@users.noreply.github.com>
2022-07-25 19:24:00 -0400

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,3 +1,5 @@
-const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'; // lower index == higher priority for token import
+const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'; 
+
+// lower index == higher priority for token import
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [COMPOUND_LIST];
