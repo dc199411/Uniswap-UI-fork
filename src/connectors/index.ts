399f009d886deb2a13e323cfee9606bd27ba5bdc
FN-TOKENS <84870847+dc199411@users.noreply.github.com>
2022-10-27 20:00:21 -0400

    Update index.ts

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -10,7 +10,7 @@ const REACT_APP_NETWORK_URL = process.env.REACT_APP_NETWORK_URL;
 // const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
 // const PORTIS_ID = process.env.REACT_APP_PORTIS_ID
 
-export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '1');
+export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '137');
 
 if (typeof REACT_APP_NETWORK_URL === 'undefined') {
   throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`);
@@ -26,7 +26,7 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 4],
+  supportedChainIds: [1, 4, 137],
 });
 
 // mainnet only
@@ -40,13 +40,13 @@ export const walletconnect = new WalletConnectConnector({
 // mainnet only
 // export const fortmatic = new FortmaticConnector({
 //   apiKey: FORMATIC_KEY ?? '',
-//   chainId: 1
+//   chainId: 137
 // })
 
 // mainnet only
 // export const portis = new PortisConnector({
 //   dAppId: PORTIS_ID ?? '',
-//   networks: [1]
+//   networks: [137]
 // })
 
 // mainnet only
